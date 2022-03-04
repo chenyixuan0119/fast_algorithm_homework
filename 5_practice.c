947a90aa9f42eb8630a793e4f54b08014455abb3
chenyixuan0119 <100676477+chenyixuan0119@users.noreply.github.com>
2022-03-04 23:26:06 +0800

    Create 5_practice.c

---
+++ 5_practice.c
@@ -0,0 +1,43 @@
+#include <stdio.h>	// for printf function
+#include <stdlib.h>	// for memory allocation
+#include <time.h>	// for time calculation
+#include <math.h>	// for sine and cosine functions
+int main() {
+	// Declare all the variables
+	int k, n, N, M;
+	double *x, *yr, *yi;
+	time_t t;
+
+	// Input the number N,M
+	printf("Input the number N,M = ");
+	scanf("%d ,%d",&N,&M);
+	
+	// Locate the memory for x, yr, yi;
+	
+	
+
+	// Initial setting for x, for example, x[k] = k
+	
+	
+	
+	
+	t = clock();	
+	// yr[n]+i*yi[n] = sum(exp(-i 2 Pi k n / N)*x[k], k=0..N-1), n=0..N-1 
+
+
+
+
+
+
+
+	// output the results
+	t = clock() - t;
+	printf("%d ms for discrete Fourier Transform of %d elements\n", t, N);
+
+	// free the memory located by x, yr, yi
+	
+	
+	
+	return 100;
+}
+
